6ae4ff0b360e38f17c87b78934d3c77a2b1f191c
Risto Yrjänä <risto.yrjana@vaadin.com>
2011-08-08 06:31:32 +0000

    - put caption first, to make it easier to convert java code  - fixed NPE with null button actions

--- addon/src/main/scala/vaadin/scala/Components.scala
+++ addon/src/main/scala/vaadin/scala/Components.scala
@@ -9,28 +9,35 @@ class ButtonClickListener(action: com.vaadin.ui.Button#ClickEvent => Unit) exten
   def buttonClick(event: com.vaadin.ui.Button#ClickEvent) { action(event) }
 }
 
-class Button(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null) extends com.vaadin.ui.Button(caption, new ButtonClickListener(action)) {
+class Button(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null)
+    extends com.vaadin.ui.Button(caption) {
   setIcon(icon)
   setStyleName(style)
+  if (action != null) addListener(new ButtonClickListener(action))
 }
 
-class LinkButton(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null) extends Button(caption, action, icon) {
+class LinkButton(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null)
+    extends Button(caption, action, icon) {
   setStyleName(BaseTheme.BUTTON_LINK)
   addStyleName(style);
 }
 
-class CheckBox(caption: String = null, immediate: Boolean = false, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null) extends com.vaadin.ui.CheckBox(caption, new ButtonClickListener(action)) {
+class CheckBox(caption: String = null, immediate: Boolean = false, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null)
+    extends com.vaadin.ui.CheckBox(caption) {
   setImmediate(immediate)
   setIcon(icon)
   setStyleName(style)
+  if (action != null) addListener(new ButtonClickListener(action))
 }
 
 // icon, caption as constructor parameters?
-class Label(width: String = 100 percent, height: String = null, content: String = null, property: Property = null, contentMode: Integer = com.vaadin.ui.Label.CONTENT_DEFAULT, style: String = null) extends com.vaadin.ui.Label(content, contentMode) {
+class Label(content: String = null, width: String = 100 percent, height: String = null, property: Property = null, contentMode: Integer = com.vaadin.ui.Label.CONTENT_DEFAULT, style: String = null)
+    extends com.vaadin.ui.Label(content, contentMode) {
   setWidth(width)
   setHeight(height)
   if (property != null) setPropertyDataSource(property)
   setStyleName(style)
 }
 
-class HtmlLabel(width: String = 100 percent, height: String = null, content: String = null, property: Property = null, style: String = null) extends Label(width, height, content, property, com.vaadin.ui.Label.CONTENT_XHTML, style)
+class HtmlLabel(content: String = null, width: String = 100 percent, height: String = null, property: Property = null, style: String = null)
+  extends Label(width = width, height = height, content = content, property = property, contentMode = com.vaadin.ui.Label.CONTENT_XHTML, style = style)
