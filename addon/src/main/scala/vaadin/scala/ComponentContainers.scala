76fef91c29d7968f66450c727c0aecc133a11137
Risto Yrjänä <risto.yrjana@vaadin.com>
2011-08-08 06:30:57 +0000

    put caption first, to make it easier to convert java code

--- addon/src/main/scala/vaadin/scala/ComponentContainers.scala
+++ addon/src/main/scala/vaadin/scala/ComponentContainers.scala
@@ -7,7 +7,7 @@ class WindowCloseListener(action: com.vaadin.ui.Window#CloseEvent => Unit) exten
   def windowClose(event: com.vaadin.ui.Window#CloseEvent) { action(event) }
 }
 
-class Window(width: String = null, height: String = null, caption: String = null, content: ComponentContainer = null, modal: Boolean = false, icon: Resource = null, style: String = null) extends com.vaadin.ui.Window(caption, content) {
+class Window(caption: String = null, width: String = null, height: String = null, content: ComponentContainer = null, modal: Boolean = false, icon: Resource = null, style: String = null) extends com.vaadin.ui.Window(caption, content) {
   setWidth(width);
   setHeight(height);
   setModal(modal);
