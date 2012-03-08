4cf43cc76920b821b072d7d0472c6aefeaa5853d
Henri Kerola <hene@vaadin.com>
2012-03-08 16:18:47 +0200

    Wrapper for Button

--- addon/src/main/scala/vaadin/scala/Components.scala
+++ addon/src/main/scala/vaadin/scala/Components.scala
@@ -3,26 +3,6 @@ package vaadin.scala
 import com.vaadin.ui.themes.BaseTheme
 import com.vaadin.ui._
 
-class ButtonClickListener(action: com.vaadin.ui.Button#ClickEvent => Unit) extends com.vaadin.ui.Button.ClickListener {
-  def buttonClick(event: com.vaadin.ui.Button#ClickEvent) = action(event)
-}
-
-class Button(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null, enabled: Boolean = true)
-  extends com.vaadin.ui.Button(caption) {
-  if (icon == null) setIcon(null) else setIcon(icon.p)
-  setStyleName(style)
-  setEnabled(enabled)
-
-  if (action != null) addListener(action)
-
-  def addListener(action: com.vaadin.ui.Button#ClickEvent => Unit): Unit = addListener(new ButtonClickListener(action))
-}
-
-class LinkButton(caption: String = null, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null)
-  extends Button(caption, action, icon) {
-  setStyleName(BaseTheme.BUTTON_LINK)
-  addStyleName(style)
-}
 
 class CheckBox(caption: String = null, checked: Boolean = false, immediate: Boolean = false, action: com.vaadin.ui.Button#ClickEvent => Unit = null, icon: Resource = null, style: String = null, enabled: Boolean = true, description: String = null)
   extends com.vaadin.ui.CheckBox(caption, checked) {
@@ -32,12 +12,15 @@ class CheckBox(caption: String = null, checked: Boolean = false, immediate: Bool
   setEnabled(enabled)
   setDescription(description)
 
-  if (action != null) addListener(action)
-
-  def addListener(action: com.vaadin.ui.Button#ClickEvent => Unit): Unit = addListener(new ButtonClickListener(action))
+  // FIXME
+  //if (action != null) addListener(action)
+  
+  // FIXME
+  //def addListener(action: com.vaadin.ui.Button#ClickEvent => Unit): Unit = addListener(new ButtonClickListener(action))
 }
 
 class Link(caption: String = null, resource: Resource = null, targetName: String = null, width: Int = -1, height: Int = -1, border: Int = com.vaadin.ui.Link.TARGET_BORDER_DEFAULT)
+  extends com.vaadin.ui.Link(caption, if (resource == null) null else resource.p, targetName, width, height, border)
 
 class TextField(caption: String = null, width: String = null, height: String = null, property: com.vaadin.data.Property = null, value: Any = null, style: String = null, prompt: String = null)
   extends com.vaadin.ui.TextField(caption) {
