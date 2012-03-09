b692c754a44cf7d7bca7df4f7a1a169210269130
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-03-09 19:31:59 +0200

    Fixed formatting

--- addon/src/main/scala/vaadin/scala/Sizeable.scala
+++ addon/src/main/scala/vaadin/scala/Sizeable.scala
@@ -4,11 +4,11 @@ import com.vaadin.ui.Component
 
 package object scala {
 
-  abstract sealed class Size { def toSizes: Tuple2[String, String]}
-  
-  object Full extends Size {def toSizes = Tuple2("100%","100%")}
-  object Undefined extends Size {def toSizes = Tuple2(null, null)}
-  
+  abstract sealed class Size { def toSizes: Tuple2[String, String] }
+
+  object Full extends Size { def toSizes = Tuple2("100%", "100%") }
+  object Undefined extends Size { def toSizes = Tuple2(null, null) }
+
   class UnitExtent(value: Number) {
     def px: String = value + "px"
     def percent: String = value + "%"
@@ -25,6 +25,6 @@ package object scala {
   implicit def intToUnitExtent(value: Int): UnitExtent = new UnitExtent(value)
 
   implicit def doubleToUnitExtent(value: Double): UnitExtent = new UnitExtent(value)
-  
-  implicit def fullSizeToUnitExtend(value: Size): Tuple2[String,String] = value.toSizes
+
+  implicit def fullSizeToUnitExtend(value: Size): Tuple2[String, String] = value.toSizes
 }
