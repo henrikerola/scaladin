e3d4d4bbfa9791819cf0a590b956a1c74ea803ae
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-04-01 16:26:03 +0300

    Added test for Window. Renamed sub-window set.

--- addon/src/main/scala/vaadin/scala/Window.scala
+++ addon/src/main/scala/vaadin/scala/Window.scala
@@ -19,11 +19,11 @@ class Window(override val p: com.vaadin.ui.Window = new com.vaadin.ui.Window) ex
   def resizable_=(resizable: Boolean) = p.setResizable(resizable)
   def resizable = p.isResizable
 
-  def windows: mutable.Set[Window] = new mutable.Set[Window] {
+  def childWindows: mutable.Set[Window] = new mutable.Set[Window] {
     import scala.collection.JavaConversions.asScalaIterator
 
     def contains(key: Window) = {
-      p.getChildWindows.contains(key)
+      p.getChildWindows.contains(key.p)
     }
     def iterator: Iterator[Window] = {
       p.getChildWindows.iterator.map(WrapperRegistry.get[Window](_).get)
