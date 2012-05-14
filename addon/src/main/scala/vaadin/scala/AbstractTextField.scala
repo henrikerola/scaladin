54214a6dc3f0ae7471c0bca0074cbb758baf67a3
Henri Kerola <henri.kerola@vaadin.com>
2012-05-14 19:00:34 +0300

    Added TextChangeListener, selectAll(), cursorPosition to AbstractTextField

--- addon/src/main/scala/vaadin/scala/AbstractTextField.scala
+++ addon/src/main/scala/vaadin/scala/AbstractTextField.scala
@@ -3,9 +3,22 @@ package vaadin.scala
 import vaadin.scala.mixins.AbstractTextFieldMixin
 import vaadin.scala.listeners.BlurListeners
 import vaadin.scala.listeners.FocusListeners
+import vaadin.scala.listeners.TextChangeListener
+import com.vaadin.event.FieldEvents
 
 package mixins {
   trait AbstractTextFieldMixin extends AbstractFieldMixin
+}
+
+object AbstractTextField {
+  object TextChangeEventMode extends Enumeration {
+    import com.vaadin.ui.AbstractTextField.TextChangeEventMode._
+    val Eager = Value(EAGER.ordinal)
+    val Timeout = Value(TIMEOUT.ordinal)
+    val Lazy = Value(LAZY.ordinal)
+  }
+
+  case class TextChangeEvent(textField: AbstractTextField, text: String, cursorPosition: Int) extends Event
 }
 
 abstract class AbstractTextField(override val p: com.vaadin.ui.AbstractTextField with AbstractTextFieldMixin) extends AbstractField(p) {
@@ -26,6 +39,32 @@ abstract class AbstractTextField(override val p: com.vaadin.ui.AbstractTextField
   def columns = p.getColumns
   def columns_=(columns: Int) = p.setColumns(columns)
 
+  def selectAll() = p.selectAll()
+
+  // TODO:
+  //def selectionRange(pos: Int, length: Int) = p.setSelectionRange(pos, length)
+
+  def cursorPosition = p.getCursorPosition
+  def cursorPosition_=(cursorPosition: Int) = p.setCursorPosition(cursorPosition)
+
+  def textChangeEventMode = AbstractTextField.TextChangeEventMode(p.getTextChangeEventMode.ordinal)
+  def textChangeEventMode_=(textChangeEventMode: AbstractTextField.TextChangeEventMode.Value) = p.setTextChangeEventMode(com.vaadin.ui.AbstractTextField.TextChangeEventMode.values()(textChangeEventMode.id))
+
+  def textChangeTimeout = p.getTextChangeTimeout
+  def textChangeTimeout_=(textChangeTimeout: Int) = p.setTextChangeTimeout(textChangeTimeout)
+
+  lazy val textChangeListeners = new ListenersTrait[AbstractTextField.TextChangeEvent, TextChangeListener] {
+    override def listeners = p.getListeners(classOf[com.vaadin.event.FieldEvents.TextChangeEvent])
+    override def addListener(elem: AbstractTextField.TextChangeEvent => Unit) = p.addListener(new TextChangeListener(elem))
+    override def removeListener(elem: TextChangeListener) = p.removeListener(elem)
+  }
+
   lazy val blurListeners = new BlurListeners(p)
   lazy val focusListeners = new FocusListeners(p)
+}
+
+package listeners {
+  class TextChangeListener(val action: AbstractTextField.TextChangeEvent => Unit) extends com.vaadin.event.FieldEvents.TextChangeListener with Listener {
+    def textChange(e: com.vaadin.event.FieldEvents.TextChangeEvent) = action(AbstractTextField.TextChangeEvent(wrapperFor[AbstractTextField](e.getComponent).get, e.getText, e.getCursorPosition))
+  }
 }
---
+++ addon/src/test/scala/vaadin/scala/tests/AbstractTextFieldTests.scala
@@ -0,0 +1,80 @@
+package vaadin.scala.tests
+
+import vaadin.scala.AbstractTextField
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import org.scalatest.mock.MockitoSugar
+import vaadin.scala.mixins.AbstractTextFieldMixin
+import org.mockito.Mockito
+
+@RunWith(classOf[JUnitRunner])
+class AbstractTextFieldTests extends FunSuite with BeforeAndAfter with MockitoSugar {
+
+  class VaadinAbstractTextField extends com.vaadin.ui.AbstractTextField with AbstractTextFieldMixin {
+  }
+
+  var abstractTextField: AbstractTextField = _
+  var spy: VaadinAbstractTextField = _
+
+  before {
+    spy = Mockito.spy(new VaadinAbstractTextField)
+    abstractTextField = new AbstractTextField(spy) {}
+
+  }
+
+  test("TextChangeEventModes") {
+    assert(AbstractTextField.TextChangeEventMode.Eager.id === com.vaadin.ui.AbstractTextField.TextChangeEventMode.EAGER.ordinal)
+    assert(AbstractTextField.TextChangeEventMode.Lazy.id === com.vaadin.ui.AbstractTextField.TextChangeEventMode.LAZY.ordinal)
+    assert(AbstractTextField.TextChangeEventMode.Timeout.id === com.vaadin.ui.AbstractTextField.TextChangeEventMode.TIMEOUT.ordinal)
+  }
+  
+  test("nullSettingAllowed") {
+    assert(!abstractTextField.nullSettingAllowed)
+    
+    abstractTextField.nullSettingAllowed = true
+    assert(abstractTextField.nullSettingAllowed)
+  }
+
+  test("maxLength") {
+    assert(abstractTextField.maxLength === -1)
+
+    abstractTextField.maxLength = 10
+    assert(abstractTextField.maxLength === 10)
+  }
+
+  test("columns") {
+    assert(abstractTextField.columns === 0)
+
+    abstractTextField.columns = 5
+    assert(abstractTextField.columns === 5)
+  }
+
+  test("selectAll()") {
+    abstractTextField.selectAll()
+    Mockito.verify(spy).selectAll()
+  }
+
+  test("cursorPosition") {
+    assert(abstractTextField.cursorPosition === 0)
+
+    abstractTextField.cursorPosition = 10
+    assert(abstractTextField.cursorPosition === 10)
+  }
+
+  test("textChangeTimeout") {
+    assert(abstractTextField.textChangeTimeout === 400)
+
+    abstractTextField.textChangeTimeout = 100
+    assert(abstractTextField.textChangeTimeout === 100)
+  }
+
+  test("textChangeEventMode") {
+    assert(abstractTextField.textChangeEventMode === AbstractTextField.TextChangeEventMode.Lazy)
+
+    abstractTextField.textChangeEventMode = AbstractTextField.TextChangeEventMode.Timeout
+    assert(abstractTextField.textChangeEventMode === AbstractTextField.TextChangeEventMode.Timeout)
+  }
+
+}
