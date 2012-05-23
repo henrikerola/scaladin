c4c0da851e7010537a6ea67c06181b0e55cf2388
Henri Kerola <henri.kerola@vaadin.com>
2012-05-23 20:31:19 +0300

    Updated Vaadin

--- project/Build.scala
+++ project/Build.scala
@@ -25,7 +25,7 @@ object BuildSettings {
 }
 
 object Dependencies {
-  val vaadinVersion = "6.7.6"
+  val vaadinVersion = "6.7.9"
   val jettyVersion = "7.3.0.v20110203"
   val scalaTestVersion = "1.6.1"
   val junitVersion = "4.9"
