78eb1bfc6dd5df9829d60ebc59d567a8743ab8f8
Henri Kerola <henri.kerola@vaadin.com>
2012-06-02 16:52:53 +0300

    Removed support for adding pure Vaadin components to AbstractOrderedlayout

--- addon/src/main/scala/vaadin/scala/AbstractOrderedLayout.scala
+++ addon/src/main/scala/vaadin/scala/AbstractOrderedLayout.scala
@@ -21,19 +21,6 @@ abstract class AbstractOrderedLayout(override val p: com.vaadin.ui.AbstractOrder
     component
   }
 
-  // Allow adding com.vaadin.ui.Components too
-  def addComponent[C <: com.vaadin.ui.Component](component: C = null, ratio: Float = -1, alignment: Alignment.Value = null, index: Int = -1): C = {
-    if (index < 0)
-      p.addComponent(component)
-    else
-      p.addComponent(component, index)
-
-    if (alignment != null) p.setComponentAlignment(component, new com.vaadin.ui.Alignment(alignment.id))
-    if (ratio >= 0) p.setExpandRatio(component, ratio)
-
-    component
-  }
-
   def expandRatio(component: Component) = p.getExpandRatio(component.p)
   def expandRatio(component: Component, ratio: Float) = p.setExpandRatio(component.p, ratio)
 
