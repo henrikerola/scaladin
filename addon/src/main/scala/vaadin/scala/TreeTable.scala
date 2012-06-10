0d79ad0992d7ae48378c2c020c23e348755ac957
Henri Kerola <henri.kerola@vaadin.com>
2012-06-10 20:50:35 +0300

    New methods to TreeTable

--- addon/src/main/scala/vaadin/scala/TreeTable.scala
+++ addon/src/main/scala/vaadin/scala/TreeTable.scala
@@ -16,6 +16,17 @@ object TreeTable {
  * @author Henri Kerola / Vaadin
  */
 class TreeTable(override val p: com.vaadin.ui.TreeTable with TreeTableMixin = new com.vaadin.ui.TreeTable with TreeTableMixin)
-  extends Table(p) with ContainerHierarchical {
+  extends Table(p) with ContainerHierarchical with ExpandNotifier with CollapseNotifier {
+
+  def collapsed(itemId: Any, collapsed: Boolean) = p.setCollapsed(itemId, collapsed)
+
+  def collapsed(itemId: Any): Boolean = p.isCollapsed(itemId)
+
+  def hierarchyColumn: Option[Any] = Option(p.getHierarchyColumnId)
+  def hierarchyColumn_=(hierarchyColumn: Option[Any]) = p.setHierarchyColumn(hierarchyColumn.getOrElse(null))
+  def hierarchyColumn_=(hierarchyColumn: Any) = p.setHierarchyColumn(hierarchyColumn)
+
+  def animationsEnabled: Boolean = p.isAnimationsEnabled
+  def animationsEnabled_=(animationsEnabled: Boolean) = p.setAnimationsEnabled(animationsEnabled)
 
 }
