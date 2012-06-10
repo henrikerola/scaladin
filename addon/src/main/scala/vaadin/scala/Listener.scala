9c54d1b831151ba06bba991b6fe6dbb8d78df94a
Henri Kerola <henri.kerola@vaadin.com>
2012-06-10 20:48:26 +0300

    Expand and collapse listeners

--- addon/src/main/scala/vaadin/scala/Listener.scala
+++ addon/src/main/scala/vaadin/scala/Listener.scala
@@ -1,6 +1,8 @@
 package vaadin.scala
 
 import scala.collection.mutable
+import vaadin.scala.listeners.ExpandListener
+import vaadin.scala.listeners.CollapseListener
 
 trait Listener {
   def action: Any
@@ -40,6 +42,26 @@ trait ListenersTrait[E, L <: Listener] extends mutable.Set[E => Unit] {
 case class BlurEvent(component: Component) extends Event
 case class FocusEvent(component: Component) extends Event
 
+case class ExpandEvent(component: Component, itemId: Any) extends Event
+
+trait ExpandNotifier { self: { def p: { def getListeners(eventType: Class[_]): java.util.Collection[_]; def addListener(listener: com.vaadin.ui.Tree.ExpandListener); def removeListener(listener: com.vaadin.ui.Tree.ExpandListener); } } =>
+  lazy val expandListeners = new ListenersTrait[ExpandEvent, ExpandListener] {
+    override def listeners = p.getListeners(classOf[com.vaadin.ui.Tree.ExpandEvent])
+    override def addListener(elem: ExpandEvent => Unit) = p.addListener(new ExpandListener(elem))
+    override def removeListener(elem: ExpandListener) = p.removeListener(elem)
+  }
+}
+
+case class CollapseEvent(component: Component, itemId: Any) extends Event
+
+trait CollapseNotifier { self: { def p: { def getListeners(eventType: Class[_]): java.util.Collection[_]; def addListener(listener: com.vaadin.ui.Tree.CollapseListener); def removeListener(listener: com.vaadin.ui.Tree.CollapseListener); } } =>
+  lazy val collapseListeners = new ListenersTrait[CollapseEvent, CollapseListener] {
+    override def listeners = p.getListeners(classOf[com.vaadin.ui.Tree.CollapseEvent])
+    override def addListener(elem: CollapseEvent => Unit) = p.addListener(new CollapseListener(elem))
+    override def removeListener(elem: CollapseListener) = p.removeListener(elem)
+  }
+}
+
 package listeners {
 
   class BlurListener(val action: BlurEvent => Unit) extends com.vaadin.event.FieldEvents.BlurListener with Listener {
@@ -60,5 +82,13 @@ package listeners {
     override def listeners = p.getListeners(classOf[com.vaadin.event.FieldEvents.FocusEvent])
     override def addListener(elem: FocusEvent => Unit) = p.addListener(new FocusListener(elem))
     override def removeListener(elem: FocusListener) = p.removeListener(elem)
+  }
+
+  class ExpandListener(val action: ExpandEvent => Unit) extends com.vaadin.ui.Tree.ExpandListener with Listener {
+    def nodeExpand(e: com.vaadin.ui.Tree.ExpandEvent) = action(ExpandEvent(wrapperFor[Component](e.getComponent).get, e.getItemId))
+  }
+
+  class CollapseListener(val action: CollapseEvent => Unit) extends com.vaadin.ui.Tree.CollapseListener with Listener {
+    def nodeCollapse(e: com.vaadin.ui.Tree.CollapseEvent) = action(CollapseEvent(wrapperFor[Component](e.getComponent).get, e.getItemId))
   }
 }
