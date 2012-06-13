f57d1f3f3d1aeaa269755834bb390c8a7e59b56a
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-06-13 21:05:15 +0300

    Change ItemClickListener to use vaadin.scala.item

--- addon/src/main/scala/vaadin/scala/ItemClickNotifier.scala
+++ addon/src/main/scala/vaadin/scala/ItemClickNotifier.scala
@@ -2,8 +2,7 @@ package vaadin.scala
 
 import vaadin.scala.internal.ItemClickListener
 
-// TODO: use Scaladin Item instead of com.vaadin.data.Item
-case class ItemClickEvent(component: Component, item: com.vaadin.data.Item, itemId: Any, propertyId: Option[Any], button: Int, clientX: Int, clientY: Int, relativeX: Int, relativeY: Int, doubleClick: Boolean, altKey: Boolean, ctrlKey: Boolean, metaKey: Boolean, shiftKey: Boolean) extends AbstractClickEvent(component, button, clientX, clientY, relativeX, relativeY, doubleClick, altKey, ctrlKey, metaKey, shiftKey)
+case class ItemClickEvent(component: Component, item: Item, itemId: Any, propertyId: Option[Any], button: Int, clientX: Int, clientY: Int, relativeX: Int, relativeY: Int, doubleClick: Boolean, altKey: Boolean, ctrlKey: Boolean, metaKey: Boolean, shiftKey: Boolean) extends AbstractClickEvent(component, button, clientX, clientY, relativeX, relativeY, doubleClick, altKey, ctrlKey, metaKey, shiftKey)
 
 trait ItemClickNotifier { self: { def p: { def getListeners(eventType: Class[_]): java.util.Collection[_]; def addListener(listener: com.vaadin.event.ItemClickEvent.ItemClickListener); def removeListener(listener: com.vaadin.event.ItemClickEvent.ItemClickListener); } } =>
   lazy val itemClickListeners = new ListenersTrait[ItemClickEvent, ItemClickListener] {
@@ -15,6 +14,6 @@ trait ItemClickNotifier { self: { def p: { def getListeners(eventType: Class[_])
 
 package internal {
   class ItemClickListener(val action: ItemClickEvent => Unit) extends com.vaadin.event.ItemClickEvent.ItemClickListener with Listener {
-    def itemClick(e: com.vaadin.event.ItemClickEvent) = action(ItemClickEvent(wrapperFor[Table](e.getComponent).get, e.getItem, e.getItemId, Option(e.getPropertyId), e.getButton, e.getClientX, e.getClientY, e.getRelativeX, e.getRelativeY, e.isDoubleClick, e.isAltKey, e.isCtrlKey, e.isMetaKey, e.isShiftKey))
+    def itemClick(e: com.vaadin.event.ItemClickEvent): Unit = action(ItemClickEvent(wrapperFor[Table](e.getComponent).get, new BasicItem(e.getItem), e.getItemId, Option(e.getPropertyId), e.getButton, e.getClientX, e.getClientY, e.getRelativeX, e.getRelativeY, e.isDoubleClick, e.isAltKey, e.isCtrlKey, e.isMetaKey, e.isShiftKey))
   }
 }
