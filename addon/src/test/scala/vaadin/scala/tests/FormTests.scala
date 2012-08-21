020b97cb563eea2c9bc8878550280b04cf49798e
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-08-21 15:14:38 -0400

    Add tests for Form and Buffered

--- addon/src/test/scala/vaadin/scala/tests/FormTests.scala
+++ addon/src/test/scala/vaadin/scala/tests/FormTests.scala
@@ -7,6 +7,7 @@ import org.scalatest.BeforeAndAfter
 import org.scalatest.mock.MockitoSugar
 import vaadin.scala._
 import org.mockito.Mockito._
+import scala.reflect.BeanProperty
 
 @RunWith(classOf[JUnitRunner])
 class FormTests extends FunSuite with BeforeAndAfter with MockitoSugar {
@@ -67,7 +68,7 @@ class FormTests extends FunSuite with BeforeAndAfter with MockitoSugar {
     assert(footer.p === form.footer.p)
   }
 
-  test("item datasource"){
+  test("item datasource") {
     val item = new IndexedContainer().addItem('id).get
     form.item = item
     assert(form.item.get.p === item.p)
@@ -75,7 +76,7 @@ class FormTests extends FunSuite with BeforeAndAfter with MockitoSugar {
     form.item = Some(item)
     assert(form.item.get.p === item.p)
   }
-  
+
   test("item datasource, None") {
     form.item = None
     assert(form.item == None)
@@ -86,32 +87,57 @@ class FormTests extends FunSuite with BeforeAndAfter with MockitoSugar {
     container.addContainerProperty("test2", classOf[String])
     container.addContainerProperty("test1", classOf[String])
     form.item = container.addItem('id)
-    form.visibleItemProperties = Seq("test1","test2")
+    form.visibleItemProperties = Seq("test1", "test2")
     assert("test1" === form.visibleItemProperties.head)
     assert("test2" === form.visibleItemProperties.tail.head)
   }
-  
+
   test("form layout") {
     assert(form.layout.isInstanceOf[FormLayout])
     form.layout = new VerticalLayout
     assert(form.layout.isInstanceOf[VerticalLayout])
   }
-  
+
   test("add field option") {
     val field = new TextField
     //these shouldn't throw exceptions
-    form.addField(None,None)
-    form.addField(Some('id),None)
+    form.addField(None, None)
+    form.addField(Some('id), None)
     form.addField(None, Some(new TextField))
     form.addField(Some('id), Some(field))
-    
+
     assert(1 === form.layout.components.size)
-    assert(Some(field) === form.field('id) )
+    assert(Some(field) === form.field('id))
   }
-  
+
+  test("commit") {
+    assert(form.commit.isValid)
+
+    val field = new TextField { value = 'value }
+    field.validators += (_ => Valid)
+    form.addField('id, field)
+    assert(form.commit.isValid)
+
+    field.validators += (_ => Invalid(List()))
+    assert(!form.commit.isValid)
+  }
+
+  test("writeThrough") {
+    form.writeThrough = false
+    val person = Person("test", "tester")
+    form.item = new BeanItem(person)
+    form.field("firstName").foreach(_.value = "newValue")
+
+    assert("test" === person.firstName)
+    assert(form.commit.isValid)
+    assert("newValue" === person.firstName)
+  }
+
   private def assertSameFactory(expected: FormFieldFactory, result: Option[FormFieldFactory]) = {
     assert(result.isDefined, "No fieldfactory as result")
     assert(expected === result.get)
     assert(expected.p === result.get.p)
   }
+
+  case class Person(@BeanProperty var firstName: String, @BeanProperty var lastName: String)
 }
