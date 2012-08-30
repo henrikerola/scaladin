5dc456b7441ab82ba5eddb8c82f7a66c98797195
Henri Kerola <henri.kerola@vaadin.com>
2012-08-30 21:03:02 +0300

    Added TablecolumnGenerators + some tests

--- addon/src/main/scala/vaadin/scala/internal/generators.scala
+++ addon/src/main/scala/vaadin/scala/internal/generators.scala
@@ -3,13 +3,17 @@ package vaadin.scala.internal
 import vaadin.scala.Table
 import vaadin.scala.Listener
 import vaadin.scala.Tree
+import vaadin.scala.Component
 
 class ColumnReorderListener(val action: Table.ColumnReorderEvent => Unit) extends com.vaadin.ui.Table.ColumnReorderListener with Listener {
   def columnReorder(e: com.vaadin.ui.Table.ColumnReorderEvent) = action(Table.ColumnReorderEvent(wrapperFor[Table](e.getComponent).get))
 }
 
-class TableColumnGenerator(val action: Table.ColumnGenerationEvent => Any) extends com.vaadin.ui.Table.ColumnGenerator with Listener {
-  def generateCell(table: com.vaadin.ui.Table, itemId: Any, columnId: Any): Object = action(Table.ColumnGenerationEvent(wrapperFor[Table](table).get, itemId, columnId)).asInstanceOf[AnyRef]
+class TableColumnGenerator(val action: Table.ColumnGenerationEvent => Option[Any]) extends com.vaadin.ui.Table.ColumnGenerator with Listener {
+  def generateCell(table: com.vaadin.ui.Table, itemId: Any, columnId: Any): Object = action(Table.ColumnGenerationEvent(wrapperFor[Table](table).get, itemId, columnId)).orNull match {
+    case c: Component => c.p
+    case a => a.asInstanceOf[AnyRef]
+  }
 }
 
 class CellStyleGenerator(val generator: Table.CellStyleGenerationEvent => Option[String]) extends com.vaadin.ui.Table.CellStyleGenerator {
