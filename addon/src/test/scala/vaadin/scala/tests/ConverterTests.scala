a3f573e6185531fd5e6d19b87bacddd3dfc3082b
Henri Kerola <henri.kerola@vaadin.com>
2012-10-03 15:53:40 +0300

    Converter related

---
+++ addon/src/test/scala/vaadin/scala/tests/ConverterTests.scala
@@ -0,0 +1,42 @@
+package vaadin.scala.tests
+
+import vaadin.scala._
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import java.util.Date
+import java.util.Locale
+
+@RunWith(classOf[JUnitRunner])
+class ConverterTests extends FunSuite {
+
+  class MyConverter extends Converter[String, Date] {
+    override def convertToModel(value: String, locale: Locale): Date = {
+      new Date
+    }
+    override def convertToPresentation(value: Date, locale: Locale): String = {
+      ""
+    }
+  }
+
+  test("modelType") {
+    val myConverter = new MyConverter
+    assert(myConverter.modelType === classOf[Date])
+  }
+
+  test("p.getModelType") {
+    val myConverter = new MyConverter
+    assert(myConverter.p.getModelType === classOf[Date])
+  }
+
+  test("presentationType") {
+    val myConverter = new MyConverter
+    assert(myConverter.presentationType === classOf[String])
+  }
+  
+  test("p.getPresentationType") {
+    val myConverter = new MyConverter
+    assert(myConverter.p.getPresentationType === classOf[String])
+  }
+
+}
