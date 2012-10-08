5001b46880d75aee80f1b0379f85966149ab2a53
Henri Kerola <henri.kerola@vaadin.com>
2012-10-08 21:41:57 +0300

    Better fix for the previous fix :)

--- addon/src/main/scala/vaadin/scala/BeanItemContainer.scala
+++ addon/src/main/scala/vaadin/scala/BeanItemContainer.scala
@@ -22,13 +22,13 @@ class BeanItemContainer[BT](override val p: com.vaadin.data.util.BeanItemContain
 
   def addBean(bean: BT): BeanItem[BT] = new BeanItem[BT](bean)
 
-  def wrapItem(unwrapped: com.vaadin.data.Item): Item = unwrapped match {
+  def wrapItem(unwrapped: com.vaadin.data.Item): Item = {
     // must create BeanItem with the constructor that takes a Vaadin BeanItem not a bean.
-    case beanItem: com.vaadin.data.util.BeanItem[_] => new BeanItem[Any](beanItem)
+    new BeanItem[BT](unwrapped.asInstanceOf[com.vaadin.data.util.BeanItem[BT]])
   }
 }
 
-class BeanItem[BT](override val p: com.vaadin.data.util.BeanItem[_]) extends Item {
+class BeanItem[BT](override val p: com.vaadin.data.util.BeanItem[BT]) extends Item {
 
   def this(bean: BT) {
     this(new com.vaadin.data.util.BeanItem[BT](bean))
@@ -38,5 +38,5 @@ class BeanItem[BT](override val p: com.vaadin.data.util.BeanItem[_]) extends Ite
     this(new com.vaadin.data.util.BeanItem[BT](bean, propertyIds.asJavaCollection))
   }
 
-  val bean = p.getBean
+  val bean: BT = p.getBean
 }
