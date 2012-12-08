cf8109478c8f59228ff3d7ca1842dd387b4922cc
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-12-08 12:28:52 +0200

    Add missing methods to HierarchicalContainer

--- addon/src/main/scala/vaadin/scala/HierarchicalContainer.scala
+++ addon/src/main/scala/vaadin/scala/HierarchicalContainer.scala
@@ -6,20 +6,23 @@ package mixins {
   trait HierarchicalContainerMixin extends IndexedContainerMixin with ContainerHierarchicalMixin
 }
 
+object HierarchicalContainer {
+  def removeItemRecursively(container: Container.Hierarchical, itemId: Any): Boolean = com.vaadin.data.util.HierarchicalContainer.removeItemRecursively(container.p, itemId)
+}
+
 /**
  * @see com.vaadin.data.util.HierarchicalContainer
  * @author Henri Kerola / Vaadin
  */
 class HierarchicalContainer(override val p: com.vaadin.data.util.HierarchicalContainer with HierarchicalContainerMixin = new com.vaadin.data.util.HierarchicalContainer with HierarchicalContainerMixin)
-    extends IndexedContainer(p) with Container.Hierarchical {
-  
-  def moveAfterSibling(itemId: Any, siblingId: Any): Unit = p.moveAfterSibling(itemId, siblingId) 
+  extends IndexedContainer(p) with Container.Hierarchical {
+
+  def moveAfterSibling(itemId: Any, siblingId: Any): Unit = p.moveAfterSibling(itemId, siblingId)
 
   def removeItemRecursively(itemId: Any): Unit = p.removeItemRecursively(itemId)
-  
+
   def includeParentsWhenFiltering: Boolean = p.isIncludeParentsWhenFiltering
   def includeParentsWhenFiltering_=(includeParentsWhenFiltering: Boolean): Unit = p.setIncludeParentsWhenFiltering(includeParentsWhenFiltering)
-  
-  // TODO: static removeItemRecursively(Container.Hierarchical container, Object itemId)
+
   // TODO: protected doSort(), doFilterContainer, passesFilters
 }
