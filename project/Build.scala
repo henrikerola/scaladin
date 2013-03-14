e9cc3329a5b6a0012861ca82de224e7d22c31fe5
Henri Kerola <henri.kerola@vaadin.com>
2013-03-14 16:34:45 +0200

    Use Scala 2.10.1

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object BuildSettings {
   val buildOrganization = "vaadin.scala"
   val buildName = "Scaladin"
   val buildVersion = "3.0.0-SNAPSHOT"
-  val buildScalaVersion = "2.10.0"
+  val buildScalaVersion = "2.10.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
