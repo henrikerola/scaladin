01bf355f135909652f48c869ee1737fb10287597
Henri Kerola <henri.kerola@vaadin.com>
2013-03-17 21:56:31 +0200

    Moven ItemClickEvent to its own file

--- addon/src/main/scala/vaadin/scala/ItemClickNotifier.scala
+++ addon/src/main/scala/vaadin/scala/ItemClickNotifier.scala
@@ -3,7 +3,6 @@ package vaadin.scala
 import vaadin.scala.internal.ItemClickListener
 import vaadin.scala.internal.ListenersTrait
 
-case class ItemClickEvent(component: Component, item: Item, itemId: Any, propertyId: Option[Any], button: MouseButton.Value, clientX: Int, clientY: Int, relativeX: Int, relativeY: Int, doubleClick: Boolean, altKey: Boolean, ctrlKey: Boolean, metaKey: Boolean, shiftKey: Boolean) extends AbstractClickEvent(component, button, clientX, clientY, relativeX, relativeY, doubleClick, altKey, ctrlKey, metaKey, shiftKey)
 
 trait ItemClickNotifier { self: { def p: { def getListeners(eventType: Class[_]): java.util.Collection[_]; def addListener(listener: com.vaadin.event.ItemClickEvent.ItemClickListener); def removeListener(listener: com.vaadin.event.ItemClickEvent.ItemClickListener); } } =>
   lazy val itemClickListeners = new ListenersTrait[ItemClickEvent, ItemClickListener] {
