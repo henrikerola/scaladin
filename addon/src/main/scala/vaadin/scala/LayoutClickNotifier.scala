72ebc2149af1ceb68f6b490238a4738585a64007
Henri Kerola <henri.kerola@vaadin.com>
2013-03-17 22:30:21 +0200

    Code style fix

--- addon/src/main/scala/vaadin/scala/LayoutClickNotifier.scala
+++ addon/src/main/scala/vaadin/scala/LayoutClickNotifier.scala
@@ -3,7 +3,10 @@ package vaadin.scala
 import vaadin.scala.internal.LayoutClickListener
 import vaadin.scala.internal.ListenersTrait
 
-trait LayoutClickNotifier { self: { def p: com.vaadin.ui.AbstractLayout with com.vaadin.event.LayoutEvents.LayoutClickNotifier; } =>
+trait LayoutClickNotifier {
+
+  self: { def p: com.vaadin.ui.AbstractLayout with com.vaadin.event.LayoutEvents.LayoutClickNotifier } =>
+
   lazy val layoutClickListeners = new ListenersTrait[LayoutClickEvent, LayoutClickListener] {
     override def listeners = p.getListeners(classOf[com.vaadin.event.LayoutEvents.LayoutClickEvent])
     override def addListener(elem: LayoutClickEvent => Unit) = p.addLayoutClickListener(new LayoutClickListener(elem))
