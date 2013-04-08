fd6923ab3f303a29156176b475ea54f5f6211732
Henri Kerola <henri.kerola@vaadin.com>
2013-04-08 23:12:51 +0300

    code style fixes

--- addon/src/main/scala/vaadin/scala/DateField.scala
+++ addon/src/main/scala/vaadin/scala/DateField.scala
@@ -3,13 +3,16 @@ package vaadin.scala
 import event.{ FocusNotifier, BlurNotifier, Event }
 import vaadin.scala.mixins.DateFieldMixin
 import internal.WrapperUtil
+import java.util.TimeZone
 
 package mixins {
   trait DateFieldMixin extends AbstractFieldMixin[java.util.Date] { self: com.vaadin.ui.DateField =>
     override def handleUnparsableDateString(dateString: String): java.util.Date = {
       // FIXME: asInstanceOf
       wrapper.asInstanceOf[DateField].unparsableDateStringHandler match {
-        case Some(handler) => handler(new DateField.UnparsableDateStringEvent(WrapperUtil.wrapperFor[DateField](this).get, dateString)).orNull
+        case Some(handler) => {
+          handler(new DateField.UnparsableDateStringEvent(WrapperUtil.wrapperFor(this).get, dateString)).orNull
+        }
         case None => null
       }
     }
@@ -19,7 +22,6 @@ package mixins {
 object DateField {
   object Resolution extends Enumeration {
     import com.vaadin.shared.ui.datefield.Resolution._
-    // Support for milliseconds is dropped in Vaadin 7 so not adding here either
     val Second = Value(SECOND.ordinal)
     val Minute = Value(MINUTE.ordinal)
     val Hour = Value(HOUR.ordinal)
@@ -35,35 +37,39 @@ object DateField {
   }
 }
 
-class DateField(override val p: com.vaadin.ui.DateField with DateFieldMixin = new com.vaadin.ui.DateField with DateFieldMixin)
+class DateField(
+  override val p: com.vaadin.ui.DateField with DateFieldMixin = new com.vaadin.ui.DateField with DateFieldMixin)
     extends AbstractField[java.util.Date](p) with BlurNotifier with FocusNotifier {
 
   resolution = DateField.Resolution.Second
 
-  var unparsableDateStringHandler: Option[DateField.UnparsableDateStringEvent => Option[java.util.Date]] = Some(DateField.DefaultUnparsableDateStringHandler)
+  var unparsableDateStringHandler: Option[DateField.UnparsableDateStringEvent => Option[java.util.Date]] =
+    Some(DateField.DefaultUnparsableDateStringHandler)
   def unparsableDateStringHandler_=(handler: DateField.UnparsableDateStringEvent => Option[java.util.Date]) {
     unparsableDateStringHandler = Option(handler)
   }
 
   def resolution = DateField.Resolution(p.getResolution.ordinal)
-  def resolution_=(resolution: DateField.Resolution.Value) = p.setResolution(com.vaadin.shared.ui.datefield.Resolution.values.apply(resolution.id))
+  def resolution_=(resolution: DateField.Resolution.Value) {
+    p.setResolution(com.vaadin.shared.ui.datefield.Resolution.values.apply(resolution.id))
+  }
 
-  def dateFormat = Option(p.getDateFormat)
-  def dateFormat_=(dateFormat: Option[String]) = p.setDateFormat(dateFormat.orNull)
-  def dateFormat_=(dateFormat: String) = p.setDateFormat(dateFormat)
+  def dateFormat: Option[String] = Option(p.getDateFormat)
+  def dateFormat_=(dateFormat: Option[String]) { p.setDateFormat(dateFormat.orNull) }
+  def dateFormat_=(dateFormat: String) { p.setDateFormat(dateFormat) }
 
-  def lenient = p.isLenient
-  def lenient_=(lenient: Boolean) = p.setLenient(lenient)
+  def lenient: Boolean = p.isLenient
+  def lenient_=(lenient: Boolean) { p.setLenient(lenient) }
 
-  def showISOWeekNumbers = p.isShowISOWeekNumbers
-  def showISOWeekNumbers_=(showISOWeekNumbers: Boolean) = p.setShowISOWeekNumbers(showISOWeekNumbers)
+  def showISOWeekNumbers: Boolean = p.isShowISOWeekNumbers
+  def showISOWeekNumbers_=(showISOWeekNumbers: Boolean) { p.setShowISOWeekNumbers(showISOWeekNumbers) }
 
-  def parseErrorMessage = Option(p.getParseErrorMessage)
-  def parseErrorMessage_=(parseErrorMessage: Option[String]) = p.setParseErrorMessage(parseErrorMessage.orNull)
-  def parseErrorMessage_=(parseErrorMessage: String) = p.setParseErrorMessage(parseErrorMessage)
+  def parseErrorMessage: Option[String] = Option(p.getParseErrorMessage)
+  def parseErrorMessage_=(parseErrorMessage: Option[String]) { p.setParseErrorMessage(parseErrorMessage.orNull) }
+  def parseErrorMessage_=(parseErrorMessage: String) { p.setParseErrorMessage(parseErrorMessage) }
 
-  def timeZone = Option(p.getTimeZone)
-  def timeZone_=(timeZone: Option[java.util.TimeZone]) = p.setTimeZone(timeZone.orNull)
-  def timeZone_=(timeZone: java.util.TimeZone) = p.setTimeZone(timeZone)
+  def timeZone: Option[TimeZone] = Option(p.getTimeZone)
+  def timeZone_=(timeZone: Option[java.util.TimeZone]) { p.setTimeZone(timeZone.orNull) }
+  def timeZone_=(timeZone: java.util.TimeZone) { p.setTimeZone(timeZone) }
 
 }
