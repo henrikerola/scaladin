861533cf59a5605b794f6e9c36b520922a4a2852
Henri Kerola <henri.kerola@vaadin.com>
2013-04-24 23:26:07 +0300

    Let Image implement ClickNotifier

--- addon/src/main/scala/vaadin/scala/Image.scala
+++ addon/src/main/scala/vaadin/scala/Image.scala
@@ -2,6 +2,7 @@ package vaadin.scala
 
 import com.vaadin.ui.{ Image => VaadinImage }
 import vaadin.scala.mixins.ImageMixin
+import vaadin.scala.event.ClickNotifier
 
 package mixins {
   trait ImageMixin extends AbstractEmbeddedMixin {
@@ -14,6 +15,6 @@ package mixins {
  * @author Henri Kerola / Vaadin
  */
 class Image(override val p: VaadinImage with ImageMixin = new VaadinImage with ImageMixin)
-    extends AbstractEmbedded(p) {
+    extends AbstractEmbedded(p) with ClickNotifier {
 
 }
