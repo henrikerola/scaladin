b96903901232e1a17cccfec38ed792dbd04b6e06
Henri Kerola <henri.kerola@vaadin.com>
2013-06-16 19:34:19 +0300

    updated scala version to 2.10.2

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object BuildSettings {
   val buildOrganization = "vaadin.scala"
   val buildName = "Scaladin"
   val buildVersion = "3.0-SNAPSHOT"
-  val buildScalaVersion = "2.10.1"
+  val buildScalaVersion = "2.10.2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
