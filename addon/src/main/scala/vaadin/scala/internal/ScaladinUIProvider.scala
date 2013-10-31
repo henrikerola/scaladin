230e302eaffbfde351d9a94a77742cbfa0ebc624
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-10-31 13:47:53 +0200

    Cache UI instance used for configuration reading + test

--- addon/src/main/scala/vaadin/scala/internal/ScaladinUIProvider.scala
+++ addon/src/main/scala/vaadin/scala/internal/ScaladinUIProvider.scala
@@ -2,41 +2,61 @@ package vaadin.scala.internal
 
 import com.vaadin.server.{ UIProviderEvent, DefaultUIProvider, UIClassSelectionEvent, UICreateEvent }
 import collection.mutable
+import scala.reflect.ClassTag
+import com.vaadin.annotations.PreserveOnRefresh
+import vaadin.scala.UI
+
+object UIConfiguration extends Enumeration {
+  type ConfigurationType = Value
+  val Title, Widgetset, Theme, PreservedOnRefresh, PushMode = Value
+}
 
 class ScaladinUIProvider extends DefaultUIProvider {
 
+  import UIConfiguration._
+
   private val uiMap = mutable.Map.empty[String, vaadin.scala.UI]
+  // (ui-class, config key) -> config value
+  private val configurationCache = mutable.Map.empty[Tuple2[String, UIConfiguration.ConfigurationType], Any]
 
   private def getUiClassName(e: UIProviderEvent) =
     e.getService.getDeploymentConfiguration.getInitParameters.getProperty("ScaladinUI")
 
   private def createScaladinUiInstance(e: UIProviderEvent): vaadin.scala.UI = {
-    if (uiMap.isEmpty) {
-      val existingUI = uiMap.head._2
-      uiMap.clear
-      existingUI
-    } else {
-      val classLoader = Some(e.getService.getClassLoader).getOrElse(getClass.getClassLoader)
-      Class.forName(getUiClassName(e), true, classLoader).newInstance.asInstanceOf[vaadin.scala.UI]
+    val classLoader = Some(e.getService.getClassLoader).getOrElse(getClass.getClassLoader)
+    Class.forName(getUiClassName(e), true, classLoader).newInstance.asInstanceOf[vaadin.scala.UI]
+  }
+
+  private def getConfigurationUiInstance(e: UIProviderEvent) =
+    uiMap.getOrElseUpdate(getUiClassName(e), createScaladinUiInstance(e))
+
+  private def getConfigurationValue[T](configType: ConfigurationType, e: UICreateEvent): T = configurationCache.getOrElseUpdate((getUiClassName(e), configType), loadConfigValue(configType, e)).asInstanceOf[T]
+
+  private def loadConfigValue(configType: ConfigurationType, e: UICreateEvent) = configType match {
+    case Title => getConfigurationUiInstance(e).title.orNull
+    case Widgetset => getConfigurationUiInstance(e).widgetset.orNull
+    case Theme => getConfigurationUiInstance(e).theme
+    case PreservedOnRefresh => getConfigurationUiInstance(e).preserveOnRefresh
+    case PushMode => com.vaadin.shared.communication.PushMode.values.apply(getConfigurationUiInstance(e).pushConfiguration.pushMode.id)
+  }
+
+  override def createInstance(e: UICreateEvent): com.vaadin.ui.UI = {
+    uiMap.get(getUiClassName(e)) match {
+      case Some(cachedClass) => uiMap.remove(getUiClassName(e)).get.p.asInstanceOf[com.vaadin.ui.UI]
+      case None => createScaladinUiInstance(e).p.asInstanceOf[com.vaadin.ui.UI]
     }
   }
 
-  private def getScaladinUiInstance(e: UIProviderEvent) =
-    uiMap.getOrElseUpdate(getUiClassName(e), createScaladinUiInstance(e))
-
-  override def createInstance(e: UICreateEvent): com.vaadin.ui.UI =
-    createScaladinUiInstance(e).p.asInstanceOf[com.vaadin.ui.UI]
-
   override def getUIClass(e: UIClassSelectionEvent): Class[_ <: com.vaadin.ui.UI] = classOf[WrappedVaadinUI]
 
-  override def getPageTitle(e: UICreateEvent) = getScaladinUiInstance(e).title.orNull
+  override def getPageTitle(e: UICreateEvent): String = getConfigurationValue(Title, e)
 
-  override def getWidgetset(e: UICreateEvent) = getScaladinUiInstance(e).widgetset.orNull
+  override def getWidgetset(e: UICreateEvent): String = getConfigurationValue(Widgetset, e)
 
-  override def getTheme(e: UICreateEvent) = getScaladinUiInstance(e).theme
+  override def getTheme(e: UICreateEvent): String = getConfigurationValue(Theme, e)
 
-  override def isPreservedOnRefresh(e: UICreateEvent) = getScaladinUiInstance(e).preserveOnRefresh
+  override def isPreservedOnRefresh(e: UICreateEvent): Boolean = getConfigurationValue(PreservedOnRefresh, e)
 
-  override def getPushMode(e: UICreateEvent) = com.vaadin.shared.communication.PushMode.values.apply(getScaladinUiInstance(e).pushConfiguration.pushMode.id)
+  override def getPushMode(e: UICreateEvent): com.vaadin.shared.communication.PushMode = getConfigurationValue(PushMode, e)
 
 }
---
+++ addon/src/test/scala/vaadin/scala/tests/ScaladinUIProviderTest.scala
@@ -0,0 +1,48 @@
+package vaadin.scala.tests
+
+import vaadin.scala.internal.ScaladinUIProvider
+import com.vaadin.server.UICreateEvent
+import com.vaadin.server.VaadinRequest
+import vaadin.scala._
+import org.mockito.Mockito._
+
+class ScaladinUIProviderTest extends ScaladinTestSuite {
+
+  test("configuration value caching") {
+    val provider = new ScaladinUIProvider
+    val mockRequest = mock[VaadinRequest]
+    val event = mock[UICreateEvent](RETURNS_DEEP_STUBS)
+
+    when(event.getService.getDeploymentConfiguration.getInitParameters.getProperty("ScaladinUI")).thenReturn(classOf[UIProviderTestUI].getCanonicalName())
+    when(event.getService.getClassLoader).thenReturn(this.getClass().getClassLoader())
+
+    val title = provider.getPageTitle(event)
+    assert("title" === title)
+    assert("theme" === provider.getTheme(event))
+    assert("widgetset" === provider.getWidgetset(event))
+    assert(true === provider.isPreservedOnRefresh(event))
+    assert(com.vaadin.shared.communication.PushMode.AUTOMATIC === provider.getPushMode(event))
+
+    assert(1 === UIProviderTestUI.created)
+    
+    provider.createInstance(event)
+
+    assert(1 === UIProviderTestUI.created)
+
+    assert("theme" === provider.getTheme(event))
+
+    assert(1 === UIProviderTestUI.created)
+  }
+}
+
+object UIProviderTestUI {
+  var created = 0
+}
+
+class UIProviderTestUI() extends UI(title = "title", theme = "theme", widgetset = "widgetset", preserveOnRefresh = true, pushMode = PushMode.Automatic) {
+
+  override def delayedInit(body: => Unit) = {
+    UIProviderTestUI.created = UIProviderTestUI.created + 1
+    super.delayedInit(body)
+  }
+}
