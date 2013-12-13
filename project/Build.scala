bda3f6c5aa83527e368fbb68a9e321e28d7efbc0
Henri Kerola <henri.kerola@vaadin.com>
2013-12-13 11:27:03 +0200

    Version 3.0.0.

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ import com.typesafe.sbt.SbtScalariform._
 object BuildSettings {
   val buildOrganization = "vaadin.scala"
   val buildName = "Scaladin"
-  val buildVersion = "3.0-SNAPSHOT"
+  val buildVersion = "3.0.0"
   val buildScalaVersion = "2.10.3"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
