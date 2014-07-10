ec86b9bd3d460b2ebc3adca8887b26cab30019f9
Henri Kerola <henri.kerola@vaadin.com>
2014-07-10 10:35:08 -0700

    Minor changes to Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,20 +8,29 @@ object Dependencies {
   private val mockitoVersion = "1.9.5"
 
   private def scalaReflect(scalaVersion: String) = "org.scala-lang" % "scala-reflect" % scalaVersion
-  private def scalaActors(scalaVersion: String) = "org.scala-lang" % "scala-actors" % scalaVersion % "test"
   private val vaadin = "com.vaadin" % "vaadin-server" % vaadinVersion
   private val vaadinClientCompiled = "com.vaadin" % "vaadin-client-compiled" % vaadinVersion
   private val vaadinThemes = "com.vaadin" % "vaadin-themes" % vaadinVersion
   private val servletApi = "javax.servlet" % "servlet-api" % "2.4"
   private val portletApi = "javax.portlet" % "portlet-api" % "2.0"
-  private val jetty = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion % "container"
-  private val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
-  private val junitInterface = "com.novocode" % "junit-interface" % "0.7" % "test->default"
-  private val mockito = "org.mockito" % "mockito-all" % mockitoVersion % "test"
+  private val jetty = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion
+  private val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+  private val junitInterface = "com.novocode" % "junit-interface" % "0.7"
+  private val mockito = "org.mockito" % "mockito-all" % mockitoVersion
 
-  def addonDeps(scalaVersion: String) = {
-    Seq(scalaActors(scalaVersion), scalaReflect(scalaVersion), vaadin, servletApi, portletApi, scalaTest, junitInterface, mockito)
-  }
+  def addonDeps(scalaVersion: String) = Seq(
+    scalaReflect(scalaVersion),
+    vaadin,
+    servletApi,
+    portletApi,
+    scalaTest % "test",
+    junitInterface % "test->default",
+    mockito % "test"
+  )
 
-  val demoDeps = Seq(jetty, vaadinClientCompiled, vaadinThemes)
+  val demoDeps = Seq(
+    jetty % "container",
+    vaadinClientCompiled,
+    vaadinThemes
+  )
 }
