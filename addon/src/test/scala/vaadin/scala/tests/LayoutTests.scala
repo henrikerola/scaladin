72c8d7c1954226daa12a1684a1738a6e0cf08617
Henri Kerola <henri.kerola@vaadin.com>
2014-12-06 15:05:00 -0800

    Moved VerticalLayout related tests from LayoutTests to VerticalLayoutTests

--- addon/src/test/scala/vaadin/scala/tests/LayoutTests.scala
+++ addon/src/test/scala/vaadin/scala/tests/LayoutTests.scala
@@ -10,44 +10,6 @@ import vaadin.scala.Layout.Margin
 @RunWith(classOf[JUnitRunner])
 class LayoutTests extends FunSuite {
 
-  test("VerticalLayout, default constructor") {
-    val layout = new VerticalLayout()
-    assert(layout.p.getWidth === 100)
-    assert(layout.p.getWidthUnits === Sizeable.Unit.PERCENTAGE)
-    assert(layout.p.getHeight === -1)
-    assert(layout.p.getHeightUnits === Sizeable.Unit.PIXELS)
-    assert(layout.margin === Margin(false, false, false, false))
-    assert(layout.spacing === false)
-    assert(layout.caption === None)
-    assert(layout.p.getStyleName === "")
-  }
-
-  test("VerticalLayout, add") {
-    val layout = new VerticalLayout
-
-    val label = new Label
-    layout.add(label, 2, Alignment.BottomCenter)
-
-    assert(layout.p.getComponent(0) === label.p)
-    assert(layout.p.getExpandRatio(label.p) === 2)
-    assert(layout.getAlignment(label) === Alignment.BottomCenter)
-    assert(layout.p.getComponentAlignment(label.p) === com.vaadin.ui.Alignment.BOTTOM_CENTER)
-
-    assert(layout.p.getComponentIndex(label.p) === 0)
-  }
-
-  test("VerticalLayout, add with index") {
-    val layout = new VerticalLayout
-    layout.add(new Label())
-    layout.add(new Label())
-    layout.add(new Label())
-
-    val button = new Button
-    layout.add(button, index = 2)
-
-    assert(layout.p.getComponentIndex(button.p) === 2)
-  }
-
   test("HorizontalLayout, default constructor") {
     val layout = new HorizontalLayout()
     assert(layout.p.getWidth === -1)
--- addon/src/test/scala/vaadin/scala/tests/VerticalLayoutTests.scala
+++ addon/src/test/scala/vaadin/scala/tests/VerticalLayoutTests.scala
@@ -1,5 +1,7 @@
 package vaadin.scala.tests
 
+import com.vaadin.server.Sizeable
+import vaadin.scala.Layout.Margin
 import vaadin.scala._
 
 /**
@@ -12,6 +14,44 @@ class VerticalLayoutTests extends ScaladinTestSuite {
 
   before {
     layout = new VerticalLayout
+  }
+
+  test("VerticalLayout, default constructor") {
+    val layout = new VerticalLayout()
+    assert(layout.p.getWidth === 100)
+    assert(layout.p.getWidthUnits === Sizeable.Unit.PERCENTAGE)
+    assert(layout.p.getHeight === -1)
+    assert(layout.p.getHeightUnits === Sizeable.Unit.PIXELS)
+    assert(layout.margin === Margin(false, false, false, false))
+    assert(layout.spacing === false)
+    assert(layout.caption === None)
+    assert(layout.p.getStyleName === "")
+  }
+
+  test("VerticalLayout, add") {
+    val layout = new VerticalLayout
+
+    val label = new Label
+    layout.add(label, 2, Alignment.BottomCenter)
+
+    assert(layout.p.getComponent(0) === label.p)
+    assert(layout.p.getExpandRatio(label.p) === 2)
+    assert(layout.getAlignment(label) === Alignment.BottomCenter)
+    assert(layout.p.getComponentAlignment(label.p) === com.vaadin.ui.Alignment.BOTTOM_CENTER)
+
+    assert(layout.p.getComponentIndex(label.p) === 0)
+  }
+
+  test("VerticalLayout, add with index") {
+    val layout = new VerticalLayout
+    layout.add(new Label())
+    layout.add(new Label())
+    layout.add(new Label())
+
+    val button = new Button
+    layout.add(button, index = 2)
+
+    assert(layout.p.getComponentIndex(button.p) === 2)
   }
 
   test("defaultComponentAlignment") {
