432c134434deb5693caa25d3dacc1076bed7e61f
Henri Kerola <henri.kerola@vaadin.com>
2015-01-17 19:43:13 -0800

    Fixes #63 - blur and focus listeners for NativeSelect

--- addon/src/main/scala/vaadin/scala/NativeSelect.scala
+++ addon/src/main/scala/vaadin/scala/NativeSelect.scala
@@ -1,6 +1,7 @@
 package vaadin.scala
 
 import com.vaadin.ui.{ NativeSelect => VaadinNativeSelect }
+import vaadin.scala.event.{ FocusNotifier, BlurNotifier }
 import vaadin.scala.mixins.NativeSelectMixin
 
 package mixins {
@@ -9,4 +10,4 @@ package mixins {
 
 class NativeSelect(
   override val p: VaadinNativeSelect with NativeSelectMixin = new VaadinNativeSelect with NativeSelectMixin)
-    extends AbstractSelect(p)
+    extends AbstractSelect(p) with BlurNotifier with FocusNotifier
