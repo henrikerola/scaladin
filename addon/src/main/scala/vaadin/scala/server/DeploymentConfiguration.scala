0235a91273338abe90e05e0baeb31f68bbe80d87
Henri Kerola <henri.kerola@vaadin.com>
2015-02-11 19:46:38 -0800

    Added missing getters to DeploymentConfiguration

--- addon/src/main/scala/vaadin/scala/server/DeploymentConfiguration.scala
+++ addon/src/main/scala/vaadin/scala/server/DeploymentConfiguration.scala
@@ -1,5 +1,7 @@
 package vaadin.scala.server
 
+import vaadin.scala.PushMode
+
 /**
  * @see com.vaadin.server.DeploymentConfiguration
  * @author Henri Kerola / Vaadin
@@ -12,14 +14,31 @@ trait DeploymentConfiguration {
 
   def isXsrfProtectionEnabled: Boolean = p.isXsrfProtectionEnabled
 
+  def isSyncIdCheckEnabled: Boolean = p.isSyncIdCheckEnabled
+
   def resourceCacheTime: Int = p.getResourceCacheTime
 
   def heartbeatInterval: Int = p.getHeartbeatInterval
 
+  def isSendUrlsAsParameters: Boolean = p.isSendUrlsAsParameters
+
   def isCloseIdleSessions: Boolean = p.isCloseIdleSessions
+
+  def pushMode: PushMode.Value = PushMode(p.getPushMode.ordinal)
 
   def initParameters: java.util.Properties = p.getInitParameters
 
   def getApplicationOrSystemProperty(propertyName: String, defaultValue: String): String =
     p.getApplicationOrSystemProperty(propertyName, defaultValue)
+
+  def uiClassName: Option[String] = Option(getApplicationOrSystemProperty("ScaladinUI", null))
+
+  def uiProviderClassName: Option[String] = Option(getApplicationOrSystemProperty("ScaladinUIProvider", null))
+
+  def widgetset: Option[String] = Option(p.getWidgetset(null))
+
+  def resourcesPath: Option[String] = Option(p.getResourcesPath)
+
+  def classLoaderName: Option[String] = Option(p.getClassLoaderName)
+
 }
