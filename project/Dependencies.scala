e867b2bea9743afe037f6bd8a93626bafe43d6cf
Henri Kerola <henri.kerola@vaadin.com>
2015-12-28 22:06:31 -0800

    Upped versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object Dependencies {
 
-  private val vaadinVersion = "7.5.2"
-  private val scalaTestVersion = "2.2.0"
-  private val mockitoVersion = "1.9.5"
+  private val vaadinVersion = "7.5.10"
+  private val scalaTestVersion = "2.2.4"
+  private val mockitoVersion = "1.10.19"
 
   private def scalaReflect(scalaVersion: String) = "org.scala-lang" % "scala-reflect" % scalaVersion
   private val vaadin = "com.vaadin" % "vaadin-server" % vaadinVersion
@@ -13,7 +13,7 @@ object Dependencies {
   private val servletApi = "javax.servlet" % "servlet-api" % "2.4"
   private val portletApi = "javax.portlet" % "portlet-api" % "2.0"
   private val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
-  private val junitInterface = "com.novocode" % "junit-interface" % "0.7"
+  private val junitInterface = "com.novocode" % "junit-interface" % "0.11"
   private val mockito = "org.mockito" % "mockito-all" % mockitoVersion
 
   def addonDeps(scalaVersion: String) = Seq(
